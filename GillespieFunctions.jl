28a865cbedfefb342e1a78c85c9138f66e12201e
Seshu Iyengar <seshu.iyengar@outlook.com>
2022-12-09 18:43:09 -0500

    tested notebook, edit functions for generality

--- GillespieFunctions.jl
+++ GillespieFunctions.jl
@@ -1,7 +1,7 @@
 import Random
 
 #Utility functions for Gillespie
-function CDFsample(rng::Random.AbstractRNG, weights::Vector{Float64},n::Int, weightsum::Float64)
+function CDFsample(rng::Random.AbstractRNG, weights::Vector{Float},n::Int, weightsum::Float)
     t = rand(rng) * weightsum
     i = 1
     cuw = weights[1] #current cumulative sum
@@ -16,30 +16,18 @@ function relative_error(x,y)
     abs(x-y)/y
 end
 
-function vectorreplace!(vec::Vector,index::UInt,newvalue)
-    vec[index] = newvalue
-    return vec
-end
-
-#To reduce allocations, you want to in place calc randvec and then take argmin
-function exponential_sample!(rng::Random.AbstractRNG,weights::Vector{Float64},randvec::Vector{Float64})
-    Random.rand!(rng,randvec)
-    randvec .= -1 .* log.(randvec) ./ weights
-    argmin(randvec)
-end
-
-function random_fill!(rng::Random.AbstractRNG,vec::Vector{Float64})
-    for i in 1:length(a)
-        vec[i] = rand(rng)
-    end
-    return nothing
-end
-
 #Direct Gillespie Simulation
-#Outcomes: array of size kxn (k reactions for system size n) with jumps in kth reaction
-function direct_gillespie(initialstate::Vector{Int64},params::Vector{Float64},numsteps::Int64,maxsteps::Int64,
-                      outcomes::Array{Int64},calcstor_constructor,rng::Random.AbstractRNG)
-    statevector = initialstate
+#statevector: n-length vector of unsigned integers. Input is initial condition
+#params: m-length list of numbers used in rate calculations
+#Outcomes: array of size kxn (k reactions for system size n) with jumps in kth reaction (ex. [1 0;-1 0;0 1;0 -1 ])
+#rate_calc: takes the state, params, and a k-d vector and fills vector with the rates of the system
+#calcstor: any object you want to use to calculate things: ex. a struct with a vector to calculate the means
+#updatestorage!: a function that has a method for your calcstor, calculating based on state, rates, params, and timestep
+#rng: any generator, best is Random.MersenneTwister()
+#numsteps: minimum number of times each reaction should occur before simulation stops
+#maxsteps: maximum number of steps before stopping simulation
+function direct_gillespie(statevector::Vector{UInt},params::Vector{Float},outcomes::Array{Int},rate_calc!::Function,
+                            calcstor,updatestorage!::Function,rng::Random.AbstractRNG,numsteps::UInt,maxsteps::UInt)
 
     numreactions = size(outcomes)
     reactioncounters = fill(numsteps,numreactions)
@@ -48,19 +36,19 @@ function direct_gillespie(initialstate::Vector{Int64},params::Vector{Float64},nu
     rates = Vector{Float64}(undef,numreactions)
     zerorates = Vector{Bool}(undef,numreactions)
 
-    calcstor = calcstor_constructor()
-    while (steps <= maxsteps-1) & (any(stoppingvec))
-        tt_protein_fb_rates!(statevector,params,rates)
+    while (steps <= maxsteps) & (any(stoppingvec))
+        rate_calc!(statevector,params,rates)
         zerorates .= rates .== 0
         if all(zerorates)
-            return "All rates trivially zero at state $(statevector) after $(steps) steps"
+            println("All rates trivially zero at state $(statevector) after $(steps) steps")
+            return calcstor
         end
         totalrate = sum(rates)
         reaction = CDFsample(rng,rates,numreactions,totalrate)
         timestep = log(1/rand(rng))/totalrate
         delta = view(outcomes,reaction,:)
         statevector .+= delta
-        updatestorage!(calcstor,statevector,rates,timestep)
+        updatestorage!(calcstor,statevector,rates,params,timestep)
         steps += 1
         reactioncounters[reaction] -= 1
         if reactioncounters[reaction] <= 0
