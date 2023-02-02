a0c49b845c3674054a3bc7208dabf76c29b496ba
Seshu Iyengar <seshu.iyengar@outlook.com>
2023-02-02 17:51:01 -0500

    Running constant rate simulations

--- Storage.jl
+++ Storage.jl
@@ -97,3 +97,77 @@ function updatestorage!(storage::Balance_with_Derivative,state::Vector{Int64},ra
     storage.meanF_y += (weight/weightsum)*dF_y 
     return nothing
 end
+
+mutable struct BalanceEqData_nofb
+    weightsum::Float64
+    meanx::Float64
+    meany::Float64
+    VX::Float64
+    VY::Float64
+    CXY::Float64
+    meanRpX::Float64
+    meanRmX::Float64
+    meanRpY::Float64
+    meanRmY::Float64
+    CXRmX::Float64
+    CXRpX::Float64
+    CYRmX::Float64
+    CYRpX::Float64
+    CXRmY::Float64
+    CXRpY::Float64
+    CYRmY::Float64
+    CYRpY::Float64
+
+
+    function BalanceEqData_nofb()
+        return new(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+    end
+    
+end
+
+function updatestorage!(storage::BalanceEqData_nofb,state::Vector{Int64},rates::Vector{Float64},params,weight,weightsum)
+        #States: [x,y]
+    #Rates: [RpX,RmX,RpY,RmY]
+    storage.weightsum += weight
+    rw = weight/storage.weightsum
+
+    x = state[1]
+    y = state[2]
+    RpX = rates[1]
+    RmX = rates[2]
+    RpY = rates[3]
+    RmY = rates[4]
+
+
+    dx = x - storage.meanx
+    dy = y - storage.meany
+    dRpX = RpX - storage.meanRpX
+    dRmX = RmX - storage.meanRmX
+    dRpY = RpY - storage.meanRpY
+    dRmY = RmY - storage.meanRmY
+
+    storage.meanx += (rw)*dx
+    storage.meany += (rw)*dy
+    storage.meanRpX += (rw)*dRpX
+    storage.meanRmX += (rw)*dRmX
+    storage.meanRpY += (rw)*dRpY
+    storage.meanRmY += (rw)*dRmY
+
+    xdiff = x - storage.meanx
+    ydiff = y - storage.meany
+
+    storage.VX += weight*dx*xdiff
+    storage.VY += weight*dy*ydiff
+
+    storage.CXY += weight*dx*ydiff
+
+    storage.CXRmX += weight*dRmX*xdiff
+    storage.CXRpX += weight*dRpX*xdiff
+    storage.CYRmX += weight*dRmX*ydiff
+    storage.CYRpX += weight*dRpX*ydiff
+    storage.CXRmY += weight*dRmY*xdiff
+    storage.CXRpY += weight*dRpY*xdiff
+    storage.CYRmY += weight*dRmY*ydiff
+    storage.CYRpY += weight*dRpY*ydiff
+    return nothing
+end
